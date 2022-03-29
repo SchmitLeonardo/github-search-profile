26903519f74311530a294b7911368f34479e4a9f
Leonardo Schmidt Borges <leonardo.borges@startaideia.com.br>
2022-03-29 08:48:14 -0300

    feat: create error component

---
+++ src/components/Error/index.js
@@ -0,0 +1,12 @@
+import './styles.scss';
+
+export const Error = () => {
+    return (
+        <div className="card">
+            <div className="no-data">
+                <p>User not found</p>
+                <p>Please enter an existing user</p>
+            </div>
+        </div>
+    )
+}
